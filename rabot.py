8f8c79a979c5725fae86b3a06bd1e19c59b40739
vinsmazuka <distortion1988@mail.ru>
2021-12-20 21:46:51 +0900

    доб перем users и мет send_welcome

--- rabot.py
+++ rabot.py
@@ -1,5 +1,19 @@
 import telebot
 from botconfig import bot_configuration
 
+users = {'vinsmazuka': '1712299131',
+         'firmamento_89': '1641854395'}
 bot = telebot.TeleBot(bot_configuration['TOKEN'])
+
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    if message.from_user.username in users:
+        bot.reply_to(message, 'Приветики, тебя добавили в друзья бота:)')
+        print(message.from_user.username, message.chat.id)
+    else:
+        bot.reply_to(message, 'Вы не авторизованы, обратитесь к Администратору - Ивлеву :)')
+
+
+bot.infinity_polling()
 
