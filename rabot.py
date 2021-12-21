9f457d0531957c7b201f29eb0ee1aa0e895ab19c
vinsmazuka <distortion1988@mail.ru>
2021-12-21 23:27:58 +0900

    доб ф mass_message и конструкц if name main

--- rabot.py
+++ rabot.py
@@ -1,19 +1,27 @@
 import telebot
 from botconfig import bot_configuration
 
-users = {'vinsmazuka': '1712299131',
-         'firmamento_89': '1641854395'}
+
 bot = telebot.TeleBot(bot_configuration['TOKEN'])
+users = {'vinsmazuka': '',
+         'firmamento_89': '1641854395'}
+
+
+def mass_message(message):
+    for chat_id in users.values():
+        bot.send_message(chat_id=chat_id, text=message)
 
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    if message.from_user.username in users:
-        bot.reply_to(message, 'Приветики, тебя добавили в друзья бота:)')
-        print(message.from_user.username, message.chat.id)
+    if message.from_user.username in list(users.keys()):
+        bot.send_message(message.chat.id, 'Приветики, тебя добавили в друзья бота:)')
+        users[message.from_user.username] = str(message.chat.id)
     else:
-        bot.reply_to(message, 'Вы не авторизованы, обратитесь к Администратору - Ивлеву :)')
+        bot.send_message(message.chat.id, 'Вы не авторизованы, обратитесь к Администратору')
 
 
-bot.infinity_polling()
+if __name__ == "__main__":
+    bot.infinity_polling()
+
 
