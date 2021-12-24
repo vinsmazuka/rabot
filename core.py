4319b7ed36984dffedb4ea9c665f9673da1638f5
vinsmazuka <distortion1988@mail.ru>
2021-12-24 23:55:50 +0900

    изм назв кл Workers на Worker, доб кл Schedule

--- core.py
+++ core.py
@@ -3,12 +3,14 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine, Float
 
-engine = create_engine("postgresql+psycopg2://postgres:Art1988em@localhost/rabotdb")
+engine = create_engine("postgresql+psycopg2://postgres:Art1988em@localhost/rabotdb",
+                       echo=True)
 Base = declarative_base()
 
-class Workers(Base):
+
+class Worker(Base):
     """
-    класс Работник, каждый экземляр класса содержит информацию
+    класс "Работник", каждый экземляр класса содержит информацию
     об отдельном работнике
     """
     __tablename__ = 'workers'
@@ -19,7 +21,60 @@ class Workers(Base):
     username = Column(String(50), nullable=False)
     chat_id = Column(String(50), nullable=True)
     salary = Column(Float(50), nullable=False)
-    depl_date = Column(Date(50), nullable=False)
+    deployment_date = Column(Date(), nullable=False)
+    schedule = relationship("Schedule")
+
+
+class Schedule(Base):
+    """
+    класс "График работы", каждый экземляр класса содержит информацию
+    о графике работы одного работника в одном месяце
+    """
+    __tablename__ = 'schedule'
+    id = Column(Integer, primary_key=True)
+    d1 = Column(String(50), nullable=True)
+    d2 = Column(String(50), nullable=True)
+    d3 = Column(String(50), nullable=True)
+    d4 = Column(String(50), nullable=True)
+    d5 = Column(String(50), nullable=True)
+    d6 = Column(String(50), nullable=True)
+    d7 = Column(String(50), nullable=True)
+    d8 = Column(String(50), nullable=True)
+    d9 = Column(String(50), nullable=True)
+    d10 = Column(String(50), nullable=True)
+    d11 = Column(String(50), nullable=True)
+    d12 = Column(String(50), nullable=True)
+    d13 = Column(String(50), nullable=True)
+    d14 = Column(String(50), nullable=True)
+    d15 = Column(String(50), nullable=True)
+    d16 = Column(String(50), nullable=True)
+    d17 = Column(String(50), nullable=True)
+    d18 = Column(String(50), nullable=True)
+    d19 = Column(String(50), nullable=True)
+    d20 = Column(String(50), nullable=True)
+    d21 = Column(String(50), nullable=True)
+    d22 = Column(String(50), nullable=True)
+    d23 = Column(String(50), nullable=True)
+    d24 = Column(String(50), nullable=True)
+    d25 = Column(String(50), nullable=True)
+    d26 = Column(String(50), nullable=True)
+    d27 = Column(String(50), nullable=True)
+    d28 = Column(String(50), nullable=True)
+    d29 = Column(String(50), nullable=True)
+    d30 = Column(String(50), nullable=True)
+    d31 = Column(String(50), nullable=True)
+    hours = Column(Float(50), nullable=True)
+    duties = Column(String(50), nullable=True)
+    wage = Column(Float(50), nullable=True)
+    worker_id = Column(Integer, ForeignKey('workers.id'))
+    worker = relationship("Worker")
+
+
+Base.metadata.create_all(engine)
+
+
+
+
 
 
 
