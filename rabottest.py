439f013406b480a8a172c5c2e70990d5e49a1e03
vinsmazuka <distortion1988@mail.ru>
2021-12-25 23:01:01 +0900

    доб кл TestCoreMethods и м test_read_file

---
+++ rabottest.py
@@ -0,0 +1,22 @@
+from unittest import TestCase
+from core import CsvReader
+
+
+test_list = [{'id': '', 'name': 'Артем', 'surname': 'Ивлев',
+              'patronymic': 'Владимирович', 'username': 'vinsmazuka',
+              'chat_id': '', 'salary': '24500', 'deployment_date': '01.12.2018',
+              'status': ''},
+             {'id': '', 'name': 'Ксения', 'surname': 'Закирова',
+              'patronymic': 'Раисовна', 'username': 'firmamento_89',
+              'chat_id': '', 'salary': '50000', 'deployment_date': '12.03.2016',
+              'status': ''}]
+
+
+class TestCoreMethods(TestCase):
+    def test_read_file(self):
+        """
+        Проверка корректного чтения из csv файла
+        """
+        self.assertEqual(CsvReader.read_file(), test_list)
+
+
