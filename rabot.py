1e413ea991b07bfae97d1d33f915d0dbbf1ee421
vinsmazuka <distortion1988@mail.ru>
2021-12-25 21:12:48 +0900

    доб м logging и логи

--- rabot.py
+++ rabot.py
@@ -1,7 +1,12 @@
+import logging
 import telebot
 from botconfig import bot_configuration
 
+FORMAT = '%(asctime)s - Уровень %(levelname)s в %(filename)s.%(funcName)s: %(message)s'
+logging.basicConfig(filename='log.txt', filemode='a', level=logging.INFO, format=FORMAT)
+
 bot = telebot.TeleBot(bot_configuration['TOKEN'])
+
 users = {'vinsmazuka': '1712299131',
          'firmamento_89': '1641854395'}
 
@@ -15,6 +20,7 @@ class Formatter:
         self.message = message
 
     def form(self):
+        logging.info(f'Сообщение "{self.message}" было отформатировано')
         return self.message
 
 
@@ -41,8 +47,11 @@ class Sendler:
         for key, value in users_dict.items():
             try:
                 bot.send_message(chat_id=value, text=message)
+                logging.info(f'Сообщение "{message}" было отправлено '
+                             f'пользователю {key} ')
             except telebot.apihelper.ApiTelegramException:
-                pass
+                logging.error(f'Бот не смог доставить пользователю {key} '
+                              f'сообщение "{message}"')
 
 
 @bot.message_handler(commands=['start'])
