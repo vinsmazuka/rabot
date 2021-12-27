88ed35e0a9f09b140a60098248fe28ec912ed70b
vinsmazuka <distortion1988@mail.ru>
2021-12-27 20:57:15 +0900

    изм кл Worker, доб кл DbFormatter и мет format_worker

--- core.py
+++ core.py
@@ -1,4 +1,5 @@
 import csv
+from datetime import datetime
 from sqlalchemy import Column, ForeignKey, Integer, String, Boolean, Date
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
@@ -24,11 +25,16 @@ class Worker(Base):
     patronymic = Column(String(50), nullable=True)
     username = Column(String(50), nullable=False)
     chat_id = Column(String(50), nullable=True)
-    salary = Column(Float(50), nullable=False)
+    salary = Column(Integer, nullable=False)
     deployment_date = Column(Date(), nullable=False)
     birthday = Column(Date(), nullable=False)
     status = Column(Boolean, nullable=False)
     schedule = relationship("Schedule")
+
+    def __str__(self):
+        return (f'{self.id}, {self.name}, {self.surname}, {self.patronymic}, '
+                f'{self.username}, {self.chat_id}, {self.salary}, {self.deployment_date}, '
+                f'{self.birthday}, {self.status}')
 
 
 class Schedule(Base):
@@ -104,8 +110,31 @@ class CsvReader:
             return result
 
 
+class DbFormatter:
+    """
+    форматирует данные для записи в базу данных
+    """
+    pass
+
+    @staticmethod
+    def format_worker(data):
+        """форматирует данные для записи в базу данных в таблицу 'workers',
+        параметр data - словарь, каждый элемент которого преобразуется
+        в класс Worker"""
+        result = []
+        for item in data:
+            result.append(Worker(name=str(item['name']), surname=str(item['surname']),
+                                 patronymic=str(item['patronymic']), username=str(item['username']),
+                                 chat_id='', salary=int(item['salary']),
+                                 deployment_date=datetime.strptime(item['deployment_date'],
+                                                                   "%d.%m.%Y").date(),
+                                 birthday=datetime.strptime(item['birthday'], "%d.%m.%Y").date(),
+                                 status=True))
+        print(*result)
+
+
 if __name__ == "__main__":
-    CsvReader.read_file(easygui.fileopenbox("укажите путь к файлу"))
+    DbFormatter.format_worker(CsvReader.read_file(easygui.fileopenbox("укажите путь к файлу")))
 
 
 
