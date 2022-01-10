ca58ccb039a49bd3070fb6d3e38fbaa0559c63ff
vinsmazuka <distortion1988@mail.ru>
2022-01-10 16:40:03 +0900

    изм м read_file,м format_worker, м format_schedule

--- core.py
+++ core.py
@@ -101,7 +101,7 @@ class CsvReader:
                 result = list(reader)
         except TypeError:
             logger.error(f'админ не указал путь к файлу')
-            return ''
+            return None
         else:
             logger.info(f'Осуществлено чтение данных из файла {path}')
             return result
@@ -200,87 +200,90 @@ class DbFormatter:
         1) кортеж сообщений, если данные в data не соответсвуют формату БД
         2) список корректных данных, если данные в data соответствуют формату БД
         """
-        warnings = []
-        result = []
-        today = datetime.now().date()
-        date_sample = r'^\d{2}.\d{2}.\d{4}$'
-        usname_sample = ascii_lowercase + '0123456789_'
-        for element in data:
-            row = {}
-            for key, value in element.items():
-                if key == 'patronymic':
-                    if value == '':
-                        row[key] = value
-                    elif not (value.isalpha() and value[0].isupper()):
-                        message = (f'указанное {key} "{value}" не корректно'
-                                   f'(отчество должно начинаться с заглавной буквы '
-                                   f'и должно состоять только из букв)')
-                        warnings.append(message)
-                    else:
-                        row[key] = value
-                elif key == 'name' or key == 'surname':
-                    if not (value.isalpha() and value[0].isupper()):
-                        message = (f'указанное {key} "{value}" не корректно'
-                                   '(имя/фамилия должны начинаться с заглавной буквы, '
-                                   'должны состоять только из букв и не могут быть пустой строкой)')
-                        warnings.append(message)
-                    else:
-                        row[key] = value
-                elif key == 'birthday' or key == 'deployment_date':
-                    if re.match(date_sample, value) is None:
-                        message = f'не корректный формат даты: "{value}"(корректный формат: 01.01.1988)'
-                        warnings.append(message)
-                    else:
-                        try:
-                            datetime.strptime(value, "%d.%m.%Y")
-                        except ValueError:
-                            message = f'не корректный формат даты: "{value}"(не существующая дата)'
+        if data is None:
+            return None
+        else:
+            warnings = []
+            result = []
+            today = datetime.now().date()
+            date_sample = r'^\d{2}.\d{2}.\d{4}$'
+            usname_sample = ascii_lowercase + '0123456789_'
+            for element in data:
+                row = {}
+                for key, value in element.items():
+                    if key == 'patronymic':
+                        if value == '':
+                            row[key] = value
+                        elif not (value.isalpha() and value[0].isupper()):
+                            message = (f'указанное {key} "{value}" не корректно'
+                                       f'(отчество должно начинаться с заглавной буквы '
+                                       f'и должно состоять только из букв)')
                             warnings.append(message)
                         else:
-                            if datetime.strptime(value, "%d.%m.%Y").date() > today:
-                                message = f'не корректный формат даты: "{value}"(дата больше текущей)'
+                            row[key] = value
+                    elif key == 'name' or key == 'surname':
+                        if not (value.isalpha() and value[0].isupper()):
+                            message = (f'указанное {key} "{value}" не корректно'
+                                       '(имя/фамилия должны начинаться с заглавной буквы, '
+                                       'должны состоять только из букв и не могут быть пустой строкой)')
+                            warnings.append(message)
+                        else:
+                            row[key] = value
+                    elif key == 'birthday' or key == 'deployment_date':
+                        if re.match(date_sample, value) is None:
+                            message = f'не корректный формат даты: "{value}"(корректный формат: 01.01.1988)'
+                            warnings.append(message)
+                        else:
+                            try:
+                                datetime.strptime(value, "%d.%m.%Y")
+                            except ValueError:
+                                message = f'не корректный формат даты: "{value}"(не существующая дата)'
                                 warnings.append(message)
                             else:
-                                row[key] = datetime.strptime(value, "%d.%m.%Y").date()
-                elif key == 'username':
-                    if len(value) < 5:
-                        message = (f'не корректный формат username: "{value}"'
-                                   f'({key} должно cостоять из латинских букв. '
-                                   'Количество знаков - минимум 5. '
-                                   'Возможно применять нижнее подчеркивание и цифры)')
-                        warnings.append(message)
-                    elif [x for x in value if x not in usname_sample]:
-                        message = (f'не корректный формат username: "{value}"'
-                                   '(username должно cостоять из латинских букв. '
-                                   'Количество знаков - минимум 5. '
-                                   'Возможно применять нижнее подчеркивание и цифры)')
-                        warnings.append(message)
-                    else:
-                        row[key] = value
-                elif key == 'salary':
-                    if value == '':
-                        message = f"вы не указали оклад у сотрудника '{element['surname']}'"
-                        warnings.append(message)
-                    else:
-                        new_value = value.replace(',', '.')
-                        try:
-                            float(new_value)
-                        except ValueError:
-                            message = (f'не корректный формат оклада: "{value}"'
-                                       '(оклад должен быть числом с плавающей запятой'
-                                       ' или целым числом)')
+                                if datetime.strptime(value, "%d.%m.%Y").date() > today:
+                                    message = f'не корректный формат даты: "{value}"(дата больше текущей)'
+                                    warnings.append(message)
+                                else:
+                                    row[key] = datetime.strptime(value, "%d.%m.%Y").date()
+                    elif key == 'username':
+                        if len(value) < 5:
+                            message = (f'не корректный формат username: "{value}"'
+                                       f'({key} должно cостоять из латинских букв. '
+                                       'Количество знаков - минимум 5. '
+                                       'Возможно применять нижнее подчеркивание и цифры)')
+                            warnings.append(message)
+                        elif [x for x in value if x not in usname_sample]:
+                            message = (f'не корректный формат username: "{value}"'
+                                       '(username должно cостоять из латинских букв. '
+                                       'Количество знаков - минимум 5. '
+                                       'Возможно применять нижнее подчеркивание и цифры)')
                             warnings.append(message)
                         else:
-                            row[key] = float(new_value)
-                else:
-                    message = f'не корректное название столбца: "{key}"'
-                    warnings.append(message)
-            result.append(row)
-        logger.info("данные из файла не соответствуют формату") if \
-            len(warnings) > 0 or data == ''\
-            else logger.info("данные соответствуют формату")
+                            row[key] = value
+                    elif key == 'salary':
+                        if value == '':
+                            message = f"вы не указали оклад у сотрудника '{element['surname']}'"
+                            warnings.append(message)
+                        else:
+                            new_value = value.replace(',', '.')
+                            try:
+                                float(new_value)
+                            except ValueError:
+                                message = (f'не корректный формат оклада: "{value}"'
+                                           '(оклад должен быть числом с плавающей запятой'
+                                           ' или целым числом)')
+                                warnings.append(message)
+                            else:
+                                row[key] = float(new_value)
+                    else:
+                        message = f'не корректное название столбца: "{key}"'
+                        warnings.append(message)
+                result.append(row)
+            logger.info("данные из файла не соответствуют формату") if \
+                len(warnings) > 0 or data == ''\
+                else logger.info("данные соответствуют формату")
 
-        return tuple(warnings) if len(warnings) > 0 else result
+            return tuple(warnings) if len(warnings) > 0 else result
 
     @staticmethod
     def format_schedule(data, id_list):
@@ -292,94 +295,97 @@ class DbFormatter:
         1) кортеж сообщений, если данные в data не соответсвуют формату БД
         2) список корректных данных, если данные в data соответсвуют формату БД
         """
-        now_year = datetime.now().year
-        months = ['январь', 'февраль', 'март',
-                  'апрель', 'май', 'июнь',
-                  'июль', 'август', 'сентябрь',
-                  'октябрь', 'ноябрь', 'декабрь']
-        errors = []
-        result = []
-        sample = '^[0-2][0-9]:[0-5][0-9]-[0-2][0-9]:[0-5][0-9]$'
-        year_sample = '^20[0-9][0-9]$'
-        for element in data:
-            work_hours = 0
-            row = {}
-            for key, value in element.items():
-                if key == 'worker_id':
-                    if value not in id_list:
-                        message = (f'не корректное значение id: "{value}"'
-                                   f'(работник c таким id в базе не зарегистрирован)')
-                        errors.append(message)
-                    else:
-                        row[key] = int(value)
-                elif key == 'month':
-                    if value in months:
-                        row[key] = value
-                    else:
-                        message = f'не корректное название месяца: "{value}"'
-                        errors.append(message)
-                elif key == 'year':
-                    if re.match(year_sample, value) is None:
-                        message = (f'не корректное значение года: "{value}"'
-                                   f'(год должен быть в формате "2020"'
-                                   f' и не может быть пустой строкой)')
-                        errors.append(message)
-                    elif int(value) > now_year:
-                        message = f'указан не корректный год: "{value}"(больше текущего)'
-                        errors.append(message)
-                    else:
-                        row[key] = value
-                elif key == 'wage':
-                    if value == '':
-                        row[key] = 0
-                    else:
-                        new_value = value.replace(',', '.')
-                        try:
-                            float(new_value)
-                        except ValueError:
-                            message = (f'не корректный формат зарплаты: "{value}"'
-                                       '(заплата должна быть целым числом, либо '
-                                       'числом с плавающей запятой)')
+        if data is None:
+            return None
+        else:
+            now_year = datetime.now().year
+            months = ['январь', 'февраль', 'март',
+                      'апрель', 'май', 'июнь',
+                      'июль', 'август', 'сентябрь',
+                      'октябрь', 'ноябрь', 'декабрь']
+            errors = []
+            result = []
+            sample = '^[0-2][0-9]:[0-5][0-9]-[0-2][0-9]:[0-5][0-9]$'
+            year_sample = '^20[0-9][0-9]$'
+            for element in data:
+                work_hours = 0
+                row = {}
+                for key, value in element.items():
+                    if key == 'worker_id':
+                        if value not in id_list:
+                            message = (f'не корректное значение id: "{value}"'
+                                       f'(работник c таким id в базе не зарегистрирован)')
                             errors.append(message)
                         else:
-                            row[key] = float(new_value)
-                elif re.match('^d[1-9][0-9]?$', key) is not None and \
-                        int(re.findall('[1-9][0-9]?', key)[0]) < 32:
-                    if value == '':
-                        row[key] = value
-                    elif re.match(sample, value) is None:
-                        message = (f'не корректный формат времени смены "{value}"'
-                                   '(корректный формат: "09:00-18:00")')
-                        errors.append(message)
-                    else:
-                        starth, startm, endh, endm = re.findall(r'\d{2}', value)
-                        try:
-                            start = datetime(year=1, month=1, day=1, hour=int(starth), minute=int(startm))
-                            end = datetime(year=1, month=1, day=1, hour=int(endh), minute=int(endm))
-                        except ValueError:
-                            message = (f'не корректный формат времени смены "{value}"'
-                                       '(кол-во часов должно быть меньше 24)')
+                            row[key] = int(value)
+                    elif key == 'month':
+                        if value in months:
+                            row[key] = value
+                        else:
+                            message = f'не корректное название месяца: "{value}"'
+                            errors.append(message)
+                    elif key == 'year':
+                        if re.match(year_sample, value) is None:
+                            message = (f'не корректное значение года: "{value}"'
+                                       f'(год должен быть в формате "2020"'
+                                       f' и не может быть пустой строкой)')
+                            errors.append(message)
+                        elif int(value) > now_year:
+                            message = f'указан не корректный год: "{value}"(больше текущего)'
                             errors.append(message)
                         else:
-                            if end <= start:
-                                message = (f'не корректный формат времени смены "{value}"'
-                                           '(время окончания смены не может быть меньше времени ее начала)')
+                            row[key] = value
+                    elif key == 'wage':
+                        if value == '':
+                            row[key] = 0
+                        else:
+                            new_value = value.replace(',', '.')
+                            try:
+                                float(new_value)
+                            except ValueError:
+                                message = (f'не корректный формат зарплаты: "{value}"'
+                                           '(заплата должна быть целым числом, либо '
+                                           'числом с плавающей запятой)')
                                 errors.append(message)
                             else:
-                                delta = end - start
-                                work_hours += delta.seconds / 3600 - 1
-                                row[key] = value
-                                row['hours'] = work_hours
-                elif key == 'surname' or key == 'name':
-                    pass
-                else:
-                    message = f'лишний столбец в шаблоне : "{key}"'
-                    errors.append(message)
-            result.append(row)
-        logger.info("данные из файла не соответствуют формату") if \
-            len(errors) > 0 or data == '' \
-            else logger.info("данные соответствуют формату")
-        return tuple(errors) if len(errors) > 0 else result
+                                row[key] = float(new_value)
+                    elif re.match('^d[1-9][0-9]?$', key) is not None and \
+                            int(re.findall('[1-9][0-9]?', key)[0]) < 32:
+                        if value == '':
+                            row[key] = value
+                        elif re.match(sample, value) is None:
+                            message = (f'не корректный формат времени смены "{value}"'
+                                       '(корректный формат: "09:00-18:00")')
+                            errors.append(message)
+                        else:
+                            starth, startm, endh, endm = re.findall(r'\d{2}', value)
+                            try:
+                                start = datetime(year=1, month=1, day=1, hour=int(starth), minute=int(startm))
+                                end = datetime(year=1, month=1, day=1, hour=int(endh), minute=int(endm))
+                            except ValueError:
+                                message = (f'не корректный формат времени смены "{value}"'
+                                           '(кол-во часов должно быть меньше 24)')
+                                errors.append(message)
+                            else:
+                                if end <= start:
+                                    message = (f'не корректный формат времени смены "{value}"'
+                                               '(время окончания смены не может быть меньше времени ее начала)')
+                                    errors.append(message)
+                                else:
+                                    delta = end - start
+                                    work_hours += delta.seconds / 3600 - 1
+                                    row[key] = value
+                                    row['hours'] = work_hours
+                    elif key == 'surname' or key == 'name':
+                        pass
+                    else:
+                        message = f'лишний столбец в шаблоне : "{key}"'
+                        errors.append(message)
+                result.append(row)
+            logger.info("данные из файла не соответствуют формату") if \
+                len(errors) > 0 or data == '' \
+                else logger.info("данные соответствуют формату")
+            return tuple(errors) if len(errors) > 0 else result
 
 
 class DbWriter:
@@ -653,9 +659,9 @@ class DbChanger:
 
 
 # if __name__ == "__main__":
-#     print(type(DbLoader.load_personalities('firmamento_89')))
-# #     # Base.metadata.create_all(engine)
-# #     # Base.metadata.drop_all(engine)
+# #     print(type(DbLoader.load_personalities('firmamento_89')))
+# # #     # Base.metadata.create_all(engine)
+# # #     # Base.metadata.drop_all(engine)
 
 
 
